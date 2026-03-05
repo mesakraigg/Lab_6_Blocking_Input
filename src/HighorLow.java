9d0e5b1a7ae8d4e1be1a53966aa7d20de66573b5
mesak <mrai3826@zenithacademy.org>
2026-03-05 15:28:38 -0500

    commit

---
+++ src/HighorLow.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+        public class HighorLow {
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+                int target = new java.util.Random().nextInt(10) + 1; 
+                int guess = 0;
+                boolean valid = false;
+
+                do {
+                    System.out.print("Guess the number [1-10]: ");
+                    if (in.hasNextInt()) {
+                        guess = in.nextInt();
+                        if (guess >= 1 && guess <= 10) valid = true;
+                        else System.out.println("Out of range.");
+                    } else {
+                        System.out.println("Invalid input: " + in.next());
+                    }
+                } while (!valid);
+
+                System.out.println("The random number was: " + target);
+                if (guess == target) System.out.println("On the money!");
+                else if (guess > target) System.out.println("High!");
+                else System.out.println("Low!");
+            }
+        }
