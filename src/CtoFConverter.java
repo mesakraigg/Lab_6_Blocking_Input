88365542b937172e687b005449c924115ef75433
mesak <mrai3826@zenithacademy.org>
2026-03-05 11:40:03 -0500

    commit

---
+++ src/CtoFConverter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CtoFConverter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double celsius = 0;
+        double fahrenheit;
+        boolean done = false;
+        String trash;
+
+        do {
+            System.out.print("Enter temperature in Celsius: ");
+            if (in.hasNextDouble()) {
+                celsius = in.nextDouble();
+                in.nextLine();
+                done = true;
+            } else {
+                trash = in.nextLine();
+                System.out.println("Invalid input: '" + trash + "'. Please enter a valid number.");
+            }
+        } while (!done);
+        
+        fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("The equivalent temperature in Fahrenheit is: " + fahrenheit);
+    }
+}
+
+
+
+
+        }
+}
