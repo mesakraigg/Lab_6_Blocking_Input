49060b18aae7d2cd5ce776f9c42b47dd395589c0
mesak <mrai3826@zenithacademy.org>
2026-03-05 14:49:18 -0500

    commit

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -19,15 +19,10 @@ public class CtoFConverter {
                 System.out.println("Invalid input: '" + trash + "'. Please enter a valid number.");
             }
         } while (!done);
-        
+
         fahrenheit = (celsius * 9 / 5) + 32;
 
         System.out.println("The equivalent temperature in Fahrenheit is: " + fahrenheit);
     }
 }
 
-
-
-
-        }
-}
